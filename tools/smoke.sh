8f4191e5aed37a8f35e58e9fc69968816c21434c
Michael Schout <mschout@gkg.net>
2018-01-03 12:27:21 -0600

    various fixups
    - sort the docker image names so we run in sorted perl version order
    - echo image name before each test run
    - bail out and display failing image name on failure

--- tools/smoke.sh
+++ tools/smoke.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-docker images | grep mschout/perl-dzil-mschout | awk '{print $1 ":" $2}' | while read image
+docker images | grep mschout/perl-dzil-mschout | awk '{print $1 ":" $2}' | sort | \
+while read image
 do
+    echo
+    echo "********** Testing in $image **********"
+    echo
+
     docker run --rm -v $PWD:/app $image /app/tools/docker-test.sh
+
+    if [ $? -ne 0 ]; then
+        echo "********** Failed in $image **********"
+        exit 1
+    fi
 done
