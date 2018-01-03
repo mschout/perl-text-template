ef62dbb13b2da23f2ccd498aabfed84588ba48e2
Michael Schout <mschout@gkg.net>
2018-01-03 12:00:56 -0600

    run through perltidy - no code changes

--- lib/Text/Template/Preprocess.pm
+++ lib/Text/Template/Preprocess.pm
@@ -10,25 +10,33 @@ use Text::Template;
 our @ISA = qw(Text::Template);
 
 sub fill_in {
-  my $self = shift;
-  my (%args) = @_;
-  my $pp = $args{PREPROCESSOR} || $self->{PREPROCESSOR} ;
-  if ($pp) {
-    local $_ = $self->source();
-    my $type = $self->{TYPE};
-#    print "# fill_in: before <$_>\n";
-    &$pp;
-#    print "# fill_in: after <$_>\n";
-    $self->set_source_data($_, $type);
-  }
-  $self->SUPER::fill_in(@_);
+    my $self   = shift;
+    my (%args) = @_;
+
+    my $pp     = $args{PREPROCESSOR} || $self->{PREPROCESSOR};
+
+    if ($pp) {
+        local $_ = $self->source();
+        my $type = $self->{TYPE};
+
+        #    print "# fill_in: before <$_>\n";
+        &$pp;
+
+        #    print "# fill_in: after <$_>\n";
+        $self->set_source_data($_, $type);
+    }
+
+    $self->SUPER::fill_in(@_);
 }
 
 sub preprocessor {
-  my ($self, $pp) = @_;
-  my $old_pp = $self->{PREPROCESSOR};
-  $self->{PREPROCESSOR} = $pp if @_ > 1;  # OK to pass $pp=undef
-  $old_pp;
+    my ($self, $pp) = @_;
+
+    my $old_pp = $self->{PREPROCESSOR};
+
+    $self->{PREPROCESSOR} = $pp if @_ > 1;    # OK to pass $pp=undef
+
+    $old_pp;
 }
 
 1;
